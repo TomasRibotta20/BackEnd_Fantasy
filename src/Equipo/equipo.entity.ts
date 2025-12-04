d45d88aa1bd164065d003ec8d1b18e603fca561a
ariel mazalan <arielmazalan11@gmail.com>
2025-12-04 01:39:12 -0300

    Equipo entity modify

--- src/Equipo/equipo.entity.ts
+++ src/Equipo/equipo.entity.ts
@@ -1,17 +1,24 @@
-import { Entity, Property, ManyToOne, OneToMany, Collection, Reference } from '@mikro-orm/core';
+import { Entity, Property, OneToOne, OneToMany, Collection } from '@mikro-orm/core';
 import { BaseEntity } from '../shared/db/baseEntity.entity.js';
-import { Users } from '../User/user.entity.js';
 import { EquipoJugador } from './equipoJugador.entity.js';
+import type { TorneoUsuario } from '../Torneo/torneoUsuario.entity.js';
 
 @Entity({ tableName: 'equipos' })
 export class Equipo extends BaseEntity {
   @Property({ nullable: false })
   nombre!: string;
 
-  // CORRECCIÓN: Usar Reference.create() para la relación con el usuario
-  // Esto es una buena práctica para evitar cargar el objeto User innecesariamente
-  @ManyToOne(() => Users, { nullable: false, unique: true })
-  usuario!: Reference<Users>;
+  @Property({ default: 0 })
+  puntos: number = 0;
+
+  @OneToOne({ 
+    entity: 'TorneoUsuario',
+    mappedBy: 'equipo'
+  })
+  torneoUsuario!: TorneoUsuario;
+
+  @Property({ default: 1000000 })
+  presupuesto: number = 1000000;
 
   // La relación ahora es con la tabla intermedia
   @OneToMany(() => EquipoJugador, (equipoJugador) => equipoJugador.equipo, {
